631e37607dd22badec4ec28a84b1f95d262ac3c9
phuong180426 <phuongphuong18426@gmail.com>
2026-05-22 17:49:21 +0700

    Thêm khung RegistrationService cho chức năng đăng ký

---
+++ MyPetClinic.Application/Services/RegistrationService.cs
@@ -0,0 +1,11 @@
+namespace MyPetClinic.Application.Services
+{
+    public class RegistrationService
+    {
+        public bool Register(string username, string password)
+        {
+            // TODO: Xử lý logic đăng ký người dùng mới
+            return true;
+        }
+    }
+}
